c6c231a63f5f5448b4c6e860c0c315e648ffd349
tonandton <www.sorasakok@gmail.com>
2022-08-12 17:17:28 +0700

    add

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
     async (user, thunkAPI) => {
         try {
             const resp = await customFetch.post("/auth/testingRegister", user);
-            // console.log(resp);
+            console.log(resp);
         } catch (error) {
             console.log(error.response);
         }
